1488806bb889ce4e79f7db5a392b72bbfebf82b4
MubeenS <sk.md.mubeen@gmail.com>
2020-10-01 09:21:45 +0530

    ci_fix3

--- esb_app/bmd_files/input_requests.sh
+++ esb_app/bmd_files/input_requests.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 curl --insecure -F "bmd_file=@bmd1.xml" https://localhost:8888/bmd
 curl --insecure -F "bmd_file=@bmd2.xml" https://localhost:8888/bmd
 curl --insecure -F "bmd_file=@bmd10.xml" https://localhost:8888/bmd
